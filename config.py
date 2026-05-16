c93e8a884c9cfc9164bf6fdaa31058ca14d69c44
TobbleCobble <tobble@mythosmc.com>
2026-05-16 10:28:02 +0800

    swapped to brevo

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2MB upload limit
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads')
-    MAIL_SERVER = 'smtp.sendgrid.net'
+    MAIL_SERVER = 'smtp-relay.brevo.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     
-    MAIL_USERNAME = 'apikey' 
-    MAIL_PASSWORD = os.environ.get('SENDGRID_API_KEY') 
+    MAIL_USERNAME = os.environ.get('BREVO_EMAIL')
+    MAIL_PASSWORD = os.environ.get('BREVO_PASSWORD') 
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
